2318ab217710b76f8fd1714cd18bfe7a58943a3c
James Batt <place.net.au@gmail.com>
2016-07-15 18:40:10 +1000

    removed url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,6 @@ module.exports = {
 		}, {
 			test: /\.css$/,
 			loader: "style-loader!css-loader"
-		}, {
-			test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-			loader: 'url-loader?limit=100000'
 		}]
 	},
 	devServer: {
